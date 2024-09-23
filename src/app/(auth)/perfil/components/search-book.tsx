ac443ae68554a2b828e559171489bae82f0a205b
Vinicius Ferreira <viniciuferreira32@gmail.com>
2024-09-23 17:42:54 -0300

    feat: creates structure for profile page

---
+++ src/app/(auth)/perfil/components/search-book.tsx
@@ -0,0 +1,5 @@
+export function SearchBook() {
+  return <div></div>
+}
+
+// TODO: criar o input de buscar livro
---
+++ src/app/(auth)/perfil/page.tsx
@@ -0,0 +1,19 @@
+import { User } from 'lucide-react'
+import type { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: 'Perfil',
+}
+
+export default function ProfilePage() {
+  return (
+    <main className="w-full pt-[4.5rem]">
+      <div className="flex h-full flex-col gap-10">
+        <h1 className="flex items-end gap-3 text-xl font-bold text-foreground">
+          <User size={24} className="size-7 text-emerald-700" /> Perfil
+        </h1>
+        <div className="flex items-start justify-between gap-20 pr-24"></div>
+      </div>
+    </main>
+  )
+}
