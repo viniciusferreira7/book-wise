151e87b585a83b208fe9e6cce0b34b14f6b57d6f
Vinicius Ferreira <viniciuferreira32@gmail.com>
2024-10-02 11:17:52 -0300

    feat: adds nuqs to better perfomance in url state

--- src/app/(auth)/explorar/components/categories.tsx
+++ src/app/(auth)/explorar/components/categories.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import type { CheckedState } from '@radix-ui/react-checkbox'
 import { useQueryState } from 'nuqs'
 
 import { Category } from './ui/category'
@@ -7,18 +8,75 @@ import { Category } from './ui/category'
 export function Categories() {
   const [categories, setCategories] = useQueryState('categories')
 
-  function handleChangeCategories(name: string){
-    setCategories((old) => )
+  function handleChangeCategories(name: string, checkedState: CheckedState) {
+    console.log({ name })
+
+    setCategories((old) => {
+      if (checkedState === true && !old) return name
+      if (checkedState === true && old) {
+        const categoriesInArray = old.split(',')
+
+        const categoryByNameIndex = categoriesInArray.findIndex(
+          (category) => category === name,
+        )
+
+        if (categoryByNameIndex >= 0) return old
+
+        if (categoryByNameIndex === -1) {
+          categoriesInArray.push(name)
+
+          return categoriesInArray.join(',')
+        }
+      }
+
+      if (checkedState === false && old) {
+        const categoriesInArray = old.split(',')
+
+        const categoryByNameIndex = categoriesInArray.findIndex(
+          (category) => category === name,
+        )
+
+        if (categoryByNameIndex >= 0) {
+          return categoriesInArray.splice(categoryByNameIndex, 1).join(',')
+        }
+      }
+
+      return old
+    })
   }
 
   return (
     <div className="flex flex-wrap gap-3">
-      <Category name="Ação" isActive onChangeCategory={handleChangeCategories}/>
-      <Category name="Terror" isActive onChangeCategory={handleChangeCategories}/>
-      <Category name="Computação" isActive onChangeCategory={handleChangeCategories}/>
-      <Category name="Ficção científica" isActive onChangeCategory={handleChangeCategories}/>
-      <Category name="Fantasia" isActive onChangeCategory={handleChangeCategories}/>
-      <Category name="Suspense" isActive onChangeCategory={handleChangeCategories}/>
+      <Category
+        name="Ação"
+        isActive={!!categories?.includes('Ação')}
+        onChangeCategory={handleChangeCategories}
+      />
+      <Category
+        name="Terror"
+        isActive={!!categories?.includes('Terror')}
+        onChangeCategory={handleChangeCategories}
+      />
+      <Category
+        name="Computação"
+        isActive={!!categories?.includes('Computação')}
+        onChangeCategory={handleChangeCategories}
+      />
+      <Category
+        name="Ficção científica"
+        isActive={!!categories?.includes('Ficção científica')}
+        onChangeCategory={handleChangeCategories}
+      />
+      <Category
+        name="Fantasia"
+        isActive={!!categories?.includes('Fantasia')}
+        onChangeCategory={handleChangeCategories}
+      />
+      <Category
+        name="Suspense"
+        isActive={!!categories?.includes('Suspense')}
+        onChangeCategory={handleChangeCategories}
+      />
     </div>
   )
 }
--- src/app/(auth)/explorar/components/ui/category.tsx
+++ src/app/(auth)/explorar/components/ui/category.tsx
@@ -1,22 +1,31 @@
+import type { CheckedState } from '@radix-ui/react-checkbox'
+
+import { Checkbox } from '@/components/ui/checkbox'
 import { cn } from '@/lib/utils'
 
 interface CategoryProps {
   name: string
   isActive: boolean
-  onChangeCategory: (name: string) => void
+  onChangeCategory: (name: string, checkedState: CheckedState) => void
 }
 
 export function Category({ name, isActive, onChangeCategory }: CategoryProps) {
   return (
-    <button
-      name={name}
-      className={cn(
-        'text-semibold text-md rounded-full border border-violet-700 bg-transparent px-4 py-[0.25rem] text-violet-700',
-        isActive && 'border-transparent bg-violet-700 text-white',
-      )}
-      onClick={() => onChangeCategory(name)}
-    >
-      {name}
-    </button>
+    <div className="flex items-center space-x-2">
+      <Checkbox
+        id={name}
+        className="hidden"
+        onCheckedChange={(checkedState) => onChangeCategory(name, checkedState)}
+      />
+      <label
+        htmlFor={name}
+        className={cn(
+          'text-semibold text-md cursor-pointer rounded-full border border-violet-700 bg-transparent px-4 py-[0.25rem] text-violet-700',
+          isActive && 'border-transparent bg-violet-700 text-white',
+        )}
+      >
+        {name}
+      </label>
+    </div>
   )
 }
